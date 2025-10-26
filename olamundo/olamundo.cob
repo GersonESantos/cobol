a5ff7b860dbec01c8ea2635ce4e6f6b0247f185f
GersonESS <gebhsantos@gmail.com>
2025-10-26 18:50:12 -0300

    a

---
+++ olamundo/olamundo.cob
@@ -0,0 +1,15 @@
+	   *> olamundo.cob - Exemplo simples "Olá, mundo!" em COBOL
+	   IDENTIFICATION DIVISION.
+	   PROGRAM-ID. OLAMUNDO.
+	   AUTHOR. GersonESantos.
+	   DATE-WRITTEN. 26/10/2025.
+
+	   ENVIRONMENT DIVISION.
+
+	   DATA DIVISION.
+	   WORKING-STORAGE SECTION.
+
+	   PROCEDURE DIVISION.
+		   DISPLAY "Olá, mundo!".
+		   STOP RUN.
+
