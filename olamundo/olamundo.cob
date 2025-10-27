a92b2c5d8d31279637dbac0c42280c8f7f3f3575
GersonESS <gebhsantos@gmail.com>
2025-10-26 23:16:01 -0300

    a

--- olamundo/olamundo.cob
+++ olamundo/olamundo.cob
@@ -1,15 +1,28 @@
-	   *> olamundo.cob - Exemplo simples "Olá, mundo!" em COBOL
-	   IDENTIFICATION DIVISION.
-	   PROGRAM-ID. OLAMUNDO.
-	   AUTHOR. GersonESantos.
-	   DATE-WRITTEN. 26/10/2025.
+	       IDENTIFICATION DIVISION.
+	       PROGRAM-ID. OLAMUNDO.
+	       AUTHOR. GersonESantos.
+	       DATE-WRITTEN. 26/10/2025.
 
-	   ENVIRONMENT DIVISION.
+	       ENVIRONMENT DIVISION.
+	       INPUT-OUTPUT SECTION.
+	       FILE-CONTROL.
+	           SELECT OUTFILE ASSIGN TO "out.txt"
+	               ORGANIZATION IS LINE SEQUENTIAL.
 
-	   DATA DIVISION.
-	   WORKING-STORAGE SECTION.
+	       DATA DIVISION.
+	       FILE SECTION.
+	       FD OUTFILE.
+	       01 OUT-REC PIC X(132).
 
-	   PROCEDURE DIVISION.
-		   DISPLAY "Olá, mundo!".
-		   STOP RUN.
+	       WORKING-STORAGE SECTION.
+	       01 WS-LINE PIC X(132) VALUE SPACES.
 
+	       PROCEDURE DIVISION.
+	           *> Diagnóstico por escrita direta em arquivo (garante saída observável)
+	           MOVE "Olá, mundo!" TO WS-LINE
+	           OPEN OUTPUT OUTFILE
+	           MOVE WS-LINE TO OUT-REC
+	           WRITE OUT-REC
+	           CLOSE OUTFILE
+	           DISPLAY "Escrito em out.txt" UPON CONSOLE.
+	           STOP RUN.
